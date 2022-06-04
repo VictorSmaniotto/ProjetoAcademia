52cf67d0b8a7b84478afac27c2027e1c69446ff8
VictorSmaniotto <victorsmaniotto@hotmail.com>
2022-06-03 22:31:20 -0300

    arquivos novos

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let btnMenu = document.getElementById("btnMenu")
+let menu = document.getElementById("menu")
+btnMenu.addEventListener("click", function(){
+    menu.classList.toggle("menuShow")
+})
