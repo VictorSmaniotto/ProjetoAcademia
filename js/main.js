ea40e8307846f9acec5f29d634310405144baa2e
VictorSmaniotto <victorsmaniotto@hotmail.com>
2022-06-13 20:24:50 -0300

    Auto-completar do form de contato

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ let menu = document.getElementById("menu");
 btnMenu.addEventListener("click",function(){
     menu.classList.toggle("menuShow");
 });
+
+const cep = document.getElementById('cep');
+
+
+cep.addEventListener('blur', function(){
+
+    fetch("https://viacep.com.br/ws/"+ this.value +"/json/").then(function(response){
+
+        response.json().then(function(endereco){
+            document.getElementById("endereco").value = endereco.logradouro;
+            document.getElementById("complemento").value = endereco.complemento;
+            document.getElementById("bairro").value = endereco.bairro;
+            document.getElementById("cidade").value = endereco.localidade;
+            document.getElementById("estado").value = endereco.uf;
+        });
+    });
+
+});
